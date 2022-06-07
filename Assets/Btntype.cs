039a929fd72408bccd28a14a631916a947a063c7
aodwn <luuflower@naver.com>
2022-06-07 22:13:08 +0900

    2022_06_07
    메인 ui수정

---
+++ Assets/Btntype.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Btntype : MonoBehaviour
+{
+    public BTNType currentType;
+    public void OnBtnClick()
+    {
+        switch (currentType)
+        {
+            case BTNType.New:
+                Debug.Log("세게임");
+                break;
+            case BTNType.Continue:
+                Debug.Log("이어하기");
+                break;
+        }
+    }
+}
---
+++ Assets/MainUI.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum BTNType 
+{
+    New,
+    Continue,
+    Option,
+    Sound,
+    Back,
+    Quit
+}
+
+
+public class MainUI : MonoBehaviour
+{
+    
+}
