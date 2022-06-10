ecb6069a8b7ffb26fa509e2472973a7aa601fd36
aodwn <luuflower@naver.com>
2022-06-10 10:40:00 +0900

    광산 게임오버 구현
    --

--- Assets/script/Countdown.cs
+++ Assets/script/Countdown.cs
@@ -7,20 +7,35 @@ public class Countdown : MonoBehaviour
 {
     [SerializeField] float setTime = 10.0f;
     [SerializeField] Text countdownText;
+
+    public GameObject panel;
     // Start is called before the first frame update
     void Start()
     {
         countdownText.text = setTime.ToString();
     }
 
+    public void uiOpen(GameObject uiPenul){
+       uiPenul.SetActive(true);
+       Debug.Log("uiOpen");
+    }
+    public void uiClose(GameObject uiPenul){
+        uiPenul.SetActive(false);
+    }
+
     // Update is called once per frame
     void Update()
     {
+       
+
         if(setTime > 0)
             setTime -= Time.deltaTime;
-        else if (setTime <= 0)
+        else if (setTime <= 0) {
             Time.timeScale = 0.0f;
+            uiOpen(panel);
+        }
 
         countdownText.text = Mathf.Round(setTime).ToString();
     }
+    
 }
