627240f103da090493fc0541cc2f3a48d64b74ed
sxxye0nz <105630255+sxxye0nz@users.noreply.github.com>
2022-09-20 00:36:16 +0900

    grfw
    djdkd

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -3,29 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
-
 {
-
-    public GameObject prfHpBar;
-    public GameObject canvas;
-
-    RectTransform hpBar;
-
-    public float height = 1.7f;
-
-    void start()
-    {
-        hpBar = Instantiate(prfHpBar, canvas.transform).GetComponent<RectTransform>();
-    }
-    void Update1()
-    {
-        Vector3 _hpBarPos =
-            Camera.main.WorldToScreenPoint(new Vector3(transform.position.x, transform.position.y + height, 0));
-        hpBar.position = _hpBarPos;
-    }
-
-    //--------------------------------------------
-
     private void Update() {
         if(hp <= 0) {
             Destroy(this.gameObject);
@@ -33,12 +11,6 @@ public class Enemy : MonoBehaviour
     }
     public int hp = 3;
    public void TakeDamage(int damage) {
-        
-
        hp = hp - damage;
    }
-
-
-
-
 }
---
+++ Assets/script/PopupSettingsBgm.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupSettingsBgm : MonoBehaviour
+{
+    public AudioSource musicsource;
+
+    public AudioSource btnsource;
+    
+    public void SetMusicVolume(float volume)
+    {
+        musicsource.volume = volume;
+    }
+
+    public void SetButtonVolume(float volume)
+    {
+        btnsource.volume = volume;
+    }
+    public void OnSfx()
+    {
+        btnsource.Play();
+    }
+}
